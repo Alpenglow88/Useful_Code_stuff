24f919e14619ba851c527734bceb8f4694445cb3
Alpenglow88 <ian.goddard@theculturetrip.com>
2020-02-19 16:18:36 +0000

    adding js page object adding code

--- to_be_organised.js
+++ to_be_organised.js
@@ -2,16 +2,37 @@
 // Check for child element being displayed within parent div
 
 const constant = PAGE.PAGE_OBJECT
-      .$(`SELECTOR`)
+      .$(`selector`)
       .isDisplayed();
 
     expect(constant).to.be.true;
 
-// Example
+// EXAMPLE
 
 const childPresent = homePage.parentDiv
       .$(`[custom-id="custom_unique_element_id"]`)
       .isDisplayed();
 
     expect(childPresent).to.be.true;
-    
+
+-------------------------------------
+// WEBDRIVERIO 5
+// Create object within page object with custom css tag
+
+{
+  get objectName() {
+    const objectName = $(`selector`);
+
+    return objectName;
+  }
+}
+
+// EXAMPLE
+{
+  get homePageElement() {
+    const homePageElement = $(`[custom-id="custom_unique_element_id"]`);
+
+    return homePageElement;
+  }
+}
+-------------------------------------
