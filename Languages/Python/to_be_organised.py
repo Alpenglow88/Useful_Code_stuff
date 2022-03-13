3ac25f0fb6e25cf1dadf3b0771ec37452ed49244
Alpenglow88 <igoddard76@gmail.com>
2022-03-13 15:35:37 +0000

    adding link for python methods

--- Languages/Python/to_be_organised.py
+++ Languages/Python/to_be_organised.py
@@ -1,2 +1,4 @@
 # https://mikemerin.github.io/Python-through-Ruby/
--------------------------------------
+# -------------------------------------
+# https://www.makeuseof.com/python-built-in-functions-methods-you-should-know/
+# -------------------------------------
